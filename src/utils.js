d28513bc93ce925f8a7a5da7d74aecd61af1963e
Yosri Mhamdi <yosri@mhamdi.co>
2022-07-31 17:49:08 +0100

    =Dyanmic css in js.

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,9 @@
 export const colors = {
   test: 'red',
 };
+
+export const setupBorder = ({ width, type, color }) => {
+  // could be some condition or a loop here.
+
+  return `${width}px ${type} ${color}`;
+};
